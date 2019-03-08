e7530e836a3862e715d6926a8af4d88a899206ab
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2019-03-08 00:16:10 -0300

    Retirado problema com rota e reformulado disposicao dos itens da lista para fins de responsividade

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { OfertaModel } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [ OfertasService ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) { }
+
+  public ofertas: OfertaModel[];
 
   ngOnInit() {
+    this.ofertas = this.ofertasService.getOfertas();
   }
 
 }
